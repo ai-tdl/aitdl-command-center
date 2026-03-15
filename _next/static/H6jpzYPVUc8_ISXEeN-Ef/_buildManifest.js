b69171838b865c5e4182be806c63869a1ff8ea80
ai-tdl <ai-tdl@users.noreply.github.com>
2026-03-15 20:49:23 +0000

    deploy: 2aca69b31cadf089218f17d3d6f316deebe627fd

---
+++ _next/static/H6jpzYPVUc8_ISXEeN-Ef/_buildManifest.js
@@ -0,0 +1,50 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/8bb07f23ccd1bdf5.js"
+  ],
+  "/404": [
+    "static/chunks/9fc2f38510079af5.js"
+  ],
+  "/_error": [
+    "static/chunks/f8a051630da102ae.js"
+  ],
+  "/about": [
+    "static/chunks/13ec6e717c55d074.js"
+  ],
+  "/about-founder": [
+    "static/chunks/284478cf142ec2e5.js"
+  ],
+  "/ai-battle": [
+    "static/chunks/3cc1b846339df0d6.js"
+  ],
+  "/compare": [
+    "static/chunks/11c92f13e3f215c9.js"
+  ],
+  "/login": [
+    "static/chunks/46b34b77ebbc9e62.js"
+  ],
+  "/profile": [
+    "static/chunks/f514bed569ae5a5f.js"
+  ],
+  "/tools/[slug]": [
+    "static/chunks/255c2bdb1f03a7f8.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/404",
+    "/_app",
+    "/_error",
+    "/about",
+    "/about-founder",
+    "/ai-battle",
+    "/compare",
+    "/login",
+    "/profile",
+    "/tools/[slug]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/02b08a8258ac3ba6.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/7a135ae590fab289.js",
+  "static/chunks/ed7e98ab8f88b7a7.js",
+  "static/chunks/2693285ab0e91410.js",
+  "static/chunks/e056548431a0f994.js",
+  "static/chunks/880df049f425b194.css",
+  "static/chunks/turbopack-ceffde8cebc44762.js"
+])
