24d67c1d7156e4228282d0a8b28d310b38c73430
ai-tdl <ai-tdl@users.noreply.github.com>
2026-03-15 16:53:07 +0000

    deploy: 8bb3d879ed0950c39068ac09efd582953961d52a

--- beta/_next/static/chunks/79756ec4cb1ee36c.js
+++ beta/_next/static/chunks/79756ec4cb1ee36c.js
@@ -1,8 +1,8 @@
 __turbopack_load_page_chunks__("/ai-battle", [
   "static/chunks/af615cbad697ca9d.js",
-  "static/chunks/c415ac42894673b4.js",
+  "static/chunks/d50a70cea296501b.js",
   "static/chunks/3833322252b952de.js",
   "static/chunks/2693285ab0e91410.js",
   "static/chunks/335f432084c21ae1.js",
-  "static/chunks/turbopack-7473fa58cf383b98.js"
+  "static/chunks/turbopack-6cd714ce734fb8b7.js"
 ])
