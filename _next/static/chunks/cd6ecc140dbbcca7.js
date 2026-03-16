f6de9528a7c6d2c1a88a325830b2dc4bef34e895
ai-tdl <ai-tdl@users.noreply.github.com>
2026-03-16 08:07:32 +0000

    deploy: 3eb86cabec9880254345ea55a8fa36bba4bf978e

---
+++ _next/static/chunks/cd6ecc140dbbcca7.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/login", [
+  "static/chunks/1a4e818a1afed86b.js",
+  "static/chunks/ed7e98ab8f88b7a7.js",
+  "static/chunks/2693285ab0e91410.js",
+  "static/chunks/4d5e07075abeb522.js",
+  "static/chunks/e056548431a0f994.js",
+  "static/chunks/turbopack-128f4c6c389fefcb.js"
+])
---
+++ _next/static/chunks/e3354c2800e3e9dd.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/profile", [
+  "static/chunks/0f5d8f1c976d42ee.js",
+  "static/chunks/ed7e98ab8f88b7a7.js",
+  "static/chunks/4d5e07075abeb522.js",
+  "static/chunks/2693285ab0e91410.js",
+  "static/chunks/e056548431a0f994.js",
+  "static/chunks/turbopack-c15dacde4a5bd9b9.js"
+])
